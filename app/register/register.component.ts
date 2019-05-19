e6cf8d8c39773fd1afc3d2c51f1714c99246c78c
Lloyd0gbonna <39203951+Lloyd0gbonna@users.noreply.github.com>
2019-05-19 16:23:17 -0400

    Update register.component.ts

--- app/register/register.component.ts
+++ app/register/register.component.ts
@@ -10,10 +10,24 @@ export class RegisterComponent  implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      'name': [this.user.name, [
+        Validators.required
+      ]],
+      'email': [this.user.email, [
+        Validators.required,
+        Validators.email
+      ]],
+      'password': [this.user.password, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(30)
+      ]]
+    });
   }
 
   onSubmit(){
-
+    alert(this.user.name + ' ' + this.user.email + ' ' + this.user.password);
   }
 
 }
