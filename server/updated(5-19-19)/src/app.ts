e29650cbd41da498042ca72ad74baba603b2e166
ajordan18 <47364232+ajordan18@users.noreply.github.com>
2019-05-21 20:15:24 -0400

    Add files via upload

--- server/updated(5-19-19)/src/app.ts
+++ server/updated(5-19-19)/src/app.ts
@@ -3,6 +3,12 @@ import { OrdersDatastore } from "./datastore";
 import * as express from 'express';
 import * as morgan from 'morgan';
 import { Request, Response } from 'express';
+import { STATUS_CODES } from "http";
+
+
+var distDir = __dirname + "/dist";
+
+var cors = require('cors');
 
 const bodyParser = require('body-parser');
 
@@ -21,17 +27,74 @@ function startServer(ordersDatastore: OrdersDatastore) {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 
-  const port = process.env.PORT || 3000;
+  app.use(express.static(distDir)); 
 
+  app.use(cors());
+
+  const port = process.env.PORT || 3000;
+  
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+
+  //GET all Users, Groups, Posts
   app.get('/users', async (request: Request, response: Response) => {
     const users = await ordersDatastore.readAllUsers();
     response.json({ users });
   });
 
+  app.get('/users/username', async (request: Request, response: Response) => {
+    const name = request.params.username;
+    const user = await ordersDatastore.readUser(name);
+    response.json({ user });
+  });
+
+  app.get('/groups', async (request: Request, response: Response) => {
+    const groups = await ordersDatastore.readAllGroups();
+    response.json({ groups });
+  });
+
+  //returns all members for all created groups
+  app.get('/groups/allmembers', async (request: Request, response: Response) => {
+    const allGroupMembers = await ordersDatastore.readAllGroupMembers();
+    response.json({ allGroupMembers });
+  });
+  
+  app.get('/posts', async (request: Request, response: Response) => {
+    const posts = await ordersDatastore.readAllPosts();
+    response.json({ posts });
+  });
+
+  app.get('/groups/largest', async (request: Request, response: Response) => {
+    const largestGroup = await ordersDatastore.getHighestPopulationGroup();
+    response.json({ largestGroup });
+  });
+
+  app.get('/groups/topthree', async (request: Request, response: Response) => {
+    const threeLargestGroups = await ordersDatastore.topThreeLargestGroups();
+    response.json({ threeLargestGroups });
+  });
+  
+  app.get('/groups/groupmembers', async (request: Request, response: Response) => {
+    const groupName = request.body.title; 
+    const membersOfGroup = await ordersDatastore.readGroupMembers(groupName);
+    response.json({ membersOfGroup });
+  });
+
+  app.get('/posts/top', async (request: Request, response: Response) => {
+    const topFivePost = await ordersDatastore.mostPopularPosts();
+    response.json({ topFivePost });
+  });
+
+
+  //Create new User, Group, Post
   app.post('/users', async (request, response) => {
     const username = request.body.username;
     const password = request.body.password;
     const email = request.body.email;
+
+    //var newUser = request.body; 
     try {
       await ordersDatastore.createUser(username, password, email);
       response.sendStatus(201);
@@ -39,7 +102,39 @@ function startServer(ordersDatastore: OrdersDatastore) {
       response.sendStatus(500);
     }
   });
+
+  app.post('/groups', async (request, response) => {
+    const title = request.body.title;
+    const description = request.body.description;
+    const creator = request.body.creator;
+    const population = 1; //new Groups have a population of 1 (the creator)
+    try {
+      await ordersDatastore.createGroup(title, description, creator, population);
+      response.sendStatus(201);
+    } catch (error) {
+      response.sendStatus(500);
+    }
+  });
+
+  app.post('/posts', async (request, response) => {
+    const title = request.body.title;
+    const content = request.body.content;
+    const author = request.body.author;
+    const likes = request.body.likes;
+    const dislikes = request.body.dislikes;
+
+    try {
+      await ordersDatastore.createPost(title, content, author, likes, dislikes);
+      response.sendStatus(201);
+    } catch (error) {
+      response.sendStatus(500);
+    }
+  });
+
+  //POST new topic
     
+
+  //Delete User, Group, Post
   app.delete('/users/:id', async (request, response) => {
     const id = request.params.id;
     try {
@@ -50,7 +145,46 @@ function startServer(ordersDatastore: OrdersDatastore) {
     }
   });
 
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+
+  //PUT or UPDATE or PATCH Functions to update documents
+  
+  //Join a Group
+
+  //Edit Profile
+
+  //Add replies/comments to topics(?)
+
+  //Like a post
+  app.patch('/posts/like/:id', async (request, response) => {
+    const id = request.params.id; 
+    try {
+      await ordersDatastore.likePost(id);
+      response.sendStatus(200);
+    } catch (error) {
+      response.sendStatus(500);
+    }
   });
+
+  //Dislike a post
+  app.patch('/posts/dislike/:id', async (request, response) => {
+    const id = request.params.id; 
+    try {
+      await ordersDatastore.dislikePost(id);
+      response.sendStatus(200);
+    } catch (error) {
+      response.sendStatus(500);
+    }
+  });
+
+  
+  app.patch('/posts/like/title', async (request, response) => {
+    const title = request.params.title; 
+    try {
+      await ordersDatastore.likePostv2(title);
+      response.sendStatus(200);
+    } catch (error) {
+      response.sendStatus(500);
+    }
+  });
+
 }
--- server/updated(5-19-19)/src/datastore.ts
+++ server/updated(5-19-19)/src/datastore.ts
@@ -1,14 +1,19 @@
-import { Collection, MongoClient, ObjectId } from 'mongodb';
+import { Collection, MongoClient, ObjectId, Decimal128, FilterQuery } from 'mongodb';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
 
 const URL = process.env.MONGO_CONNECTION || '';
 
 export class OrdersDatastore {
   users: Collection;
+  groups: Collection;
+  posts: Collection;
 
   constructor(client: MongoClient) {
     this.users = client.db().collection('users');
+    this.groups = client.db().collection('groups');
+    this.posts = client.db().collection('posts');
   }
   
   static async connect() {
@@ -21,15 +26,155 @@ export class OrdersDatastore {
       }));
   }
 
+  /*Gets Documents within Database Collection*/
+  
+  //retrieve all user JSON Objects in database
   async readAllUsers() {
     return await this.users.find({}).toArray();
   }
 
-  async createUser(username: string, password: string, email: string ) {
-    await this.users.insertOne({ username, password, email });
+  async readUser(username: string) {
+    return await this.users.find({"username": username});
+  }
+  
+  //retrieve all group JSON Objects in database
+  async readAllGroups() {
+    return await this.groups.find({}).toArray();
+  }
+  
+  //retrieve all group titles and their corresponding members in database
+  async readAllGroupMembers() {
+    var groups = await this.groups.find({}).toArray(); 
+
+    // var allMembers = groups.map(function(group) {
+    //   return {"members": group.members, "title": group.title};
+    // }) 
+    // console.log(allMembers);
+
+    const allGroupsMembers = groups.map( x => { 
+      x.title + ": " + x.members
+    }); 
+    console.log(allGroupsMembers); 
+  }
+  
+  //retrives group title of the group with largest population
+  async getHighestPopulationGroup() {
+    var largestGroup = await this.groups.find().sort({population:-1}).limit(1).toArray(); //sorts groups and returns one with the highest number
+    var largestGroupTitle = largestGroup[0].title; //assigns title attribute from JSON to variable
+    
+    return largestGroupTitle; //return largest group's title name
+    
+    //console.log(largestGroupTitle + " is currently the largest group"); //Output testing
+  }
+  
+  //returns array of group titles for the three largest groups
+  async topThreeLargestGroups() {
+    var threeLargestGroups = await this.groups.find().sort({population:-1}).limit(3).toArray();
+    
+    var topThree = [
+      threeLargestGroups[0].title,
+      threeLargestGroups[1].title,
+      threeLargestGroups[2].title
+    ]; 
+
+    return topThree; //returns array of the titles of the three largest groups
+
+    //Output testing
+    // console.log(" Three Largest Groups ");
+    // console.log("----------------------");
+    // var z = 0;
+    // var groupPop, groupTitle; 
+    // while(z < threeLargestGroups.length) {
+    //   groupPop = threeLargestGroups[z].population;
+    //   groupTitle = threeLargestGroups[z].title;
+    //   console.log(groupTitle + " group, " + groupPop + " members");
+    //   z++;
+    // }
+  }
+  
+  //returns all members of a specified group
+  async readGroupMembers(groupName: string) {
+    var groupSpecified = await this.groups.find({"title":groupName}).toArray(); //returns JSON object of specified group
+
+    var membersList = groupSpecified[0].members.toString().split(",");//takes members value within JSON Object and assigns its value into a string array
+    //console.log(membersList);   //Output testing
+    return membersList; //returns array of the specified group's members
   }
 
+  //returns all post JSON Objects in database
+  async readAllPosts() {
+    return await this.posts.find({}).toArray();
+  }
+  //returns  post with the most amount of likes
+  async mostPopularPosts() {
+    var topFivePost = await this.posts.find().sort({likes:-1}).limit(5).toArray();
+    
+    var topFivePostList = [
+      topFivePost[0].title,
+      topFivePost[1].title,
+      topFivePost[2].title,
+      topFivePost[3].title,
+      topFivePost[4].title
+    ]; 
+
+    return topFivePostList; //returns array of the titles of the five most liked posts
+  }
+ 
+
+  /*Creates Documents within Database Collection*/
+
+  //create new user within database
+  async createUser(username: string, password: string, email: string) {
+    await this.users.insertOne({ username, password, email });
+  }
+  //creates new group within database
+  async createGroup(title: string, description: string, creator: string, population: number) {
+    await this.groups.insertOne({ title, description, creator, population });
+  } 
+  //creates new post within database
+  async createPost(title: string, content: string, author: string, likes: number, dislikes: number) {
+    await this.posts.insertOne({ title, content, author, likes, dislikes });
+  }
+
+
+  /*Updates Database Collection Documents*/
+ 
+  //Increments the number of likes on a particular post
+  async likePost(id: string) {
+    await this.posts.update(
+      { _id: new ObjectId(id) }, //specifies document to be updated
+      { $inc: { likes: 1 } } //increments number of likes for that document by 1
+    );
+  }
+
+  async likePostv2(title: string) {
+    await this.posts.update(
+      {title: title}, //specifies document to be updated
+      { $inc: { likes: 1 } } //increments number of likes for that document by 1
+    );
+  }
+
+  //Increments the numer of dislikes on a particular post
+  async dislikePost(id: string) {
+    await this.posts.update(
+      { _id: new ObjectId(id) }, //specifies document to be updated
+      { $inc: { dislikes: 1 } } //increments number of dislikes for that document by 1
+    );
+  }
+
+
+  
+  /*Deletes Database Collection Documents*/
+
+  //Delete User, Group, Post
   async deleteUser(id: string) {
     await this.users.deleteOne({ _id: new ObjectId(id) });
   }
+
+  
+
+  async fetch() {
+
+  }
+
 }
